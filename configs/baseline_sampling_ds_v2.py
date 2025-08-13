58fb9bfd2ce74f452b654dd400a35178d1c06ad1
Max <maxlevin@hunterschools.org>
2025-08-13 16:39:34 -0400

    commit

---
+++ configs/baseline_sampling_ds_v2.py
@@ -0,0 +1,28 @@
+from prover.utils import AttrDict
+from prover.algorithms import Sampling
+
+# verifier
+lean_max_concurrent_requests = 8
+lean_memory_limit = 10
+lean_timeout = 120
+
+# model
+batch_size = 4
+mode = 'cot_ds_v2' # chat templates can be changed in prover/utils.py
+pass_ = 32
+model_path = 'deepseek-ai/DeepSeek-Prover-V2-7B'
+
+model_args = AttrDict(
+    mode=mode,  
+    temperature=1,
+    max_tokens=8192,
+    top_p=0.95,
+)
+
+# algorithm
+n_search_procs = 16
+sampler = dict(
+    algorithm=Sampling,
+    sample_num=pass_,
+    log_interval=32,
+)
---
+++ run.py
@@ -0,0 +1,34 @@
+import os
+from apollo import ApolloRepair
+from gemini_answer import get_gemini_sorrified_lean_sketch
+code = '''
+import Mathlib
+import Aesop
+
+set_option maxHeartbeats 0
+
+open BigOperators Real Nat Topology Rat
+'''
+
+gem_sketch = get_gemini_sorrified_lean_sketch(problem_name, nl_statement, formal_statement)
+code += '\n' + gem_sketch + '\n'
+# Parameters
+max_attempts = 2 # maximum recursion depth
+config = 'configs/baseline_sampling_ds_v2.py' # config file for LLM
+problem_dir = 'logs/test' # where to save final lean file and intermediate proof states
+
+# Instantiate the Apollo repair object
+manager = ApolloRepair(
+    code=code,
+    lemma_name='test',
+    config=config,
+    rec_depth=max_attempts,
+    log_dir = problem_dir
+)
+
+# Run the entire fix/verify pipeline and get the final assembled proof path
+final_proof_path = manager.run()
+
+with open(final_proof_path, 'r') as f:
+    proof = f.read()
+print(proof)
