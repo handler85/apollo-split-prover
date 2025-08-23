94e3ce73fb56eacd813ddb9efbb6807cf60cc909
Max Levin <mlevin742@gmail.com>
2025-08-23 16:24:22 -0400

    timeout

---
+++ handle_timeout.py
@@ -0,0 +1,38 @@
+import os
+import json
+from apollo import ApolloRepair
+from gemini_answer import get_gemini_sorrified_lean_sketch
+from o3_mini_answer import get_o3mini_sorrified_lean_sketch
+import shutil
+import sys
+
+def delete_root_logs():
+    logs_dir = os.path.join(os.getcwd(), "logs")
+    if os.path.isdir(logs_dir):
+        print(f"Deleting: {logs_dir}")
+        shutil.rmtree(logs_dir, ignore_errors=True)
+    else:
+        print("No 'logs' directory found in current directory.")
+
+def delete_pycache(root="."):
+    for dirpath, dirnames, filenames in os.walk(root, topdown=False):
+        for dirname in dirnames:
+            if dirname == "__pycache__":
+                full_path = os.path.join(dirpath, dirname)
+                print(f"Deleting: {full_path}")
+                shutil.rmtree(full_path, ignore_errors=True)
+
+
+problem_index = int(sys.argv[1])
+problem = problems[problem_index]
+problem_name = problem['name']
+logs_dir = os.path.join(os.getcwd(), "logs")
+problem_dir = os.path.join(logs_dir, problem_name)
+if os.path.isdir(problem_dir):
+    print(f"Deleting: {problem_dir}")
+    shutil.rmtree(problem_dir, ignore_errors=True)
+else:
+    print("Directory to be deleted not found in current directory.")
+delete_pycache(".")
+with open("timed_out.txt", "a") as file:
+    file.write(f"\n{problem_name}")
--- run_all.sh
+++ run_all.sh
@@ -3,7 +3,11 @@
 NUM_PROBLEMS=$(wc -l < minif2f_train.jsonl)
 for ((i=0; i<NUM_PROBLEMS; i++)); do
     echo "Processing problem $i"
-    python3 run.py $i
+    timeout 10m python3 run.py $i
+    if [ $? -eq 124 ]; then
+        echo "Timeout reached for problem $i"
+        python3 handle_timeout.py $i
+    fi
     echo "Killing all Python processes..."
     pkill -9 -f python || true
     rm -rf ~/.cache/vllm
